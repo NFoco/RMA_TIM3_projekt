1606e68601edd95591a1de8ecccc72677615fe50
NFoco <nuh.foco2021@treca-gimnazija.edu.ba>
2024-04-12 17:19:39 +0200

    Novi skrin

--- app/src/main/java/com/example/rma_tim3_projekt/MainActivity.kt
+++ app/src/main/java/com/example/rma_tim3_projekt/MainActivity.kt
@@ -7,7 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.rma_tim3_projekt.screens.PretrazivanjeScreen
+import com.example.rma_tim3_projekt.screens.PocetniEkran
+//import com.example.rma_tim3_projekt.screens.PretrazivanjeScreen
 import com.example.rma_tim3_projekt.ui.theme.RMA_TIM3_projektTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PretrazivanjeScreen()
+                    PocetniEkran()
                 }
             }
         }
---
+++ app/src/main/java/com/example/rma_tim3_projekt/screens/PocetniEkran.kt
@@ -0,0 +1,130 @@
+package com.example.rma_tim3_projekt.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.rma_tim3_projekt.R
+
+@Composable
+fun PocetniEkran () {
+
+    var text by remember {mutableStateOf("")}
+    val screenWidthInDp = 800.dp
+
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        color = Color.LightGray
+    ) {
+
+        Box(
+            modifier = Modifier.size(600.dp, 200.dp)
+        ) {
+
+        Column{
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Tražilica") }
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Box(
+                modifier = Modifier
+                    .height(180.dp)
+                    .width(screenWidthInDp / 2 - 10.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(Color(0xC8EB79FF))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.paketici),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+
+                    )
+            }
+            Text(
+                text = "Popularno",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = "Vidi sve",
+                color = Color.Yellow,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            @Composable
+            fun LazyRowExample(numbers: Array<Int>) {
+
+                // Place A lazy Row
+                LazyRow(
+                    contentPadding = PaddingValues(8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+
+                    item {
+                        RowItem(number = 0)
+                    }
+                    item {
+                        RowItem(number = 1)
+                    }
+                    item {
+                        RowItem(number = 2)
+                    }
+
+                }
+            }
+            Text(
+                text = "Vrste ljubimaca",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = "Vidi sve",
+                color = Color.Yellow,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+            )
+          }
+        }
+    }
+}
+
+
+
+
